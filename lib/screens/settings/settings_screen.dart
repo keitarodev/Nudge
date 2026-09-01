ce35a686db6d638f00e350a1a85430fb43a6cd44
Bot007Bott <chuibot5@gmail.com>
2026-09-01 19:10:40 +0700

    feat: add app navigation foundation

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Home"),
+        child: Text("Settings"),
       ),
     );
   }
