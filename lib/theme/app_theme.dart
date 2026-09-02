1f38468de085b5edb1daf44aa4eeb4bdb41be2d8
Bot007Bott <chuibot5@gmail.com>
2026-09-02 14:51:15 +0700

    feat: finalize theme colors

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -6,15 +6,28 @@ class AppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     primaryColor: AppColors.lightAccent,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    colorScheme: ColorScheme.light(
+      primary: AppColors.lightAccent,
+      surface: AppColors.lightBackground,
+      onSurface: AppColors.color1,
+    ),
     textTheme: TextTheme(
       displayLarge: AppTextStyles.heading,
       titleLarge: AppTextStyles.sectionHeading,
       bodyLarge: AppTextStyles.body,
     ),
   );
+
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     primaryColor: AppColors.darkAccent,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    colorScheme: ColorScheme.dark(
+      primary: AppColors.darkAccent,
+      surface: AppColors.darkBackground,
+      onSurface: Colors.white,
+    ),
     textTheme: TextTheme(
       displayLarge: AppTextStyles.heading,
       titleLarge: AppTextStyles.sectionHeading,
