5a0609c40449ac153422b25712bc8f19c0d0a8a4
Keii-kun <tpkeitaro1@gmail.com>
2026-09-11 23:40:45 +0700

    feat: redesign create screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import '../../theme/app_spacing.dart';
 import '../../theme/app_text_styles.dart';
-import '../../theme/app_colors.dart';
 
 class SplashScreen extends StatefulWidget {
   final VoidCallback onFinished;
